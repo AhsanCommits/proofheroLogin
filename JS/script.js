8efc8c676e67731fa5df6d84ce48699593cd8078
Ahsan Raza <ahsanraza00059@gmail.com>
2022-02-15 17:16:15 +0500

    Login Page

---
+++ JS/script.js
@@ -0,0 +1,282 @@
+var pages = {
+    'Dashboard': `        <h1>Dashboard</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card p-2">
+            <div class="card-body">
+                <div class="Container">
+                    <div class="row1">
+                        <div>
+                            <div class="info">
+                                Free Plan
+                            </div>
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-outline-danger btn-sm">Renew/Update</button>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <div>You have used <span class="Info">21%</span> impressions out of 50000 this month.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-3 p-5" id="campaign-section">
+            <div class="header">
+                <h3>Campaign</h3>
+                <div>
+                    <button type="button" class="btn btn-secondary"
+                        style="user-select: auto; background:#E4E6EB; color:#000">
+                        <i class="fa fa-filter" aria-hidden="true"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger">+ Create Campaign</button>
+                </div>
+            </div>
+            <div class="table-responsive my-3">
+                <table class="table text-center">
+                    <thead>
+                        <th>Campaign</th>
+                        <th>Status</th>
+                        <th>Launched on</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Campaign 1</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                </div>
+                            </td>
+                            <td>9th February, 2022</td>
+                            <td><button type="button" class="btn btn-secondary"
+                                    style="user-select: auto; background:#E4E6EB; color:#000; padding:0px 5px">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-three-dots" viewBox="0 0 16 16" style="user-select: auto;">
+                                        <path
+                                            d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                                            style="user-select: auto;"></path>
+                                    </svg>
+                                </button></td>
+                        </tr>
+                        <tr>
+                            <td>Campaign 1</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                </div>
+                            </td>
+                            <td>9th February, 2022</td>
+                            <td><button type="button" class="btn btn-secondary"
+                                    style="user-select: auto; background:#E4E6EB; color:#000;padding:0px 5px">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-three-dots" viewBox="0 0 16 16" style="user-select: auto;">
+                                        <path
+                                            d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                                            style="user-select: auto;"></path>
+                                    </svg>
+                                </button></td>
+
+                        </tr>
+                        <tr>
+                            <td>Campaign 1</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                </div>
+                            </td>
+                            <td>9th February, 2022</td>
+                            <td><button type="button" class="btn btn-secondary"
+                                    style="user-select: auto; background:#E4E6EB; color:#000;padding:0px 5px">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-three-dots" viewBox="0 0 16 16" style="user-select: auto;">
+                                        <path
+                                            d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                                            style="user-select: auto;"></path>
+                                    </svg>
+                                </button></td>
+
+                        </tr>
+                        <tr>
+                            <td>Campaign 1 </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                </div>
+                            </td>
+                            <td>9th February, 2022</td>
+                            <td><button type="button" class="btn btn-secondary"
+                                    style="user-select: auto; background:#E4E6EB; color:#000;padding:0px 5px">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-three-dots" viewBox="0 0 16 16" style="user-select: auto;">
+                                        <path
+                                            d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                                            style="user-select: auto;"></path>
+                                    </svg>
+                                </button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>Showing 1-2 out of 2 results</div>
+        </div>
+`,
+    'Plan': `<h1>Plan</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card mt-3">
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+        </div>`,
+    'Account': `<h1>Account</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card mt-3">
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+        </div>`,
+    'Payments': `<h1>Payments</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card mt-3">
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+        </div>`,
+    'API': `<h1>API</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card mt-3">
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+        </div>`,
+    'Affiliate': `<h1>Affiliate</h1>
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                Welcome back, AltumCode! You've successfully logged in.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card mt-3">
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+        </div>`,
+};
+function getPageContent(page) {
+    var contentToReturn;
+
+    switch (page) {
+        case 'Dashboard':
+            contentToReturn = pages.Dashboard;
+            break;
+        case 'Plan':
+            contentToReturn = pages.Plan;
+            break;
+        case 'Account':
+            contentToReturn = pages.Account;
+            break;
+        case 'Payments':
+            contentToReturn = pages.Payments;
+            break;
+        case 'API':
+            contentToReturn = pages.API;
+            break;
+        case 'Affiliate':
+            contentToReturn = pages.Affiliate;
+            break;
+
+        default:
+            contentToReturn = pages.Dashboard;
+            break;
+    }
+    document.getElementById('content').innerHTML = contentToReturn;
+}
+let sidebar = document.getElementById('sidebar');
+let text = document.querySelectorAll('.text');
+let content = document.getElementById('content');
+sidebar.addEventListener('mouseenter', e => {
+    sidebar.style.width = '18%';
+    content.style.width = '82%';
+    sidebar.style.transition = 'width 0.3s ease-in-out';
+    content.style.transition = 'width 0.3s ease-in-out';
+    text.forEach(element => {
+        element.style.display = 'inline-block';
+    });
+});
+
+sidebar.addEventListener('mouseleave', e => {
+    sidebar.style.width = '6%';
+    content.style.width = '94%';
+    sidebar.style.transition = 'width 1s ease-in-out';
+    content.style.transition = 'width 1s ease-in-out';
+    text.forEach(element => {
+        element.style.display = 'none';
+    });
+
+
+});
